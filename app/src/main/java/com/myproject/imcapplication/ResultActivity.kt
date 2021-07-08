ddcc801db1f61184db524dd12371a580b697471e
Denis Alves <dee.alves2@gmail.com>
2021-07-08 19:59:47 -0300

    Trabalhando com herança de estilos

---
+++ app/src/main/java/com/myproject/imcapplication/ResultActivity.kt
@@ -0,0 +1,13 @@
+package com.myproject.imcapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        Log.e("ciclo", "Ciclo de criação")
+    }
+}
